00f4ce241a338b5ea338e099c7c53bd6f3db1c51
Deepak Kumar Mishra <demishra@microsoft.com>
2021-06-06 21:56:50 +0530

    all about csv and excel read and write with dataframes

---
+++ pythondataframes.py
@@ -0,0 +1,8 @@
+Using Python Dictionary
+-----------------------
+weather_data = {
+    'day': ['1/1/2021','1/2/2021','1/3/2021'],
+    'temp': [32,35,28],
+    'windspeed':[6,7,2],
+    'event': ['rain','sunny','snow']
+ }
